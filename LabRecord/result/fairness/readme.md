ef20bbd7128c53e50d19b6d4314fcaa922a4cdd8
stormlin <lin-jinting@outlook.com>
2017-08-29 22:17:15 +0800

    switch to laptop, 2017-08-29 22.17.

---
+++ LabRecord/result/fairness/readme.md
@@ -0,0 +1,10 @@
+# Settings for Fairness Evaluation in Mininet
+Mininet settings for *LAN*, *International Internet* and *Domestic Internet*.
+
+Set to below:
+
+|Scenario|Bandwidth|Delay|Loss|
+|--------|---------|-----|----|
+|LAN|100M|1ms|0%|
+|Domestic Internet|1M|30ms|0.001%|
+|International Internet|100M|300ms|1%|
--- fairness.py
+++ fairness.py
@@ -25,7 +25,7 @@ def read_text_file(file_name):
     return lines
 
 
-def get_result_dictionary(intervals, keys):
+def get_result_dictionary(intervals):
     """
     Get results in a dictionary accessed by different socket keys.
 
@@ -37,32 +37,30 @@ def get_result_dictionary(intervals, keys):
     result = {}
 
     #   Create entries in dictionary for all flows
-    for i in range(len(keys)):
-        flow_table = {}
-        rtt_list = []
-        snd_cwnd_list = []
-        bits_per_second_list = []
-        retransmits_list = []
-        flow_table["rtt"] = rtt_list
-        flow_table["snd_cwnd"] = snd_cwnd_list
-        flow_table["bits_per_second"] = bits_per_second_list
-        flow_table["retransmits"] = retransmits_list
-        #   Add entry
-        result[keys[i]] = flow_table
+    flow_table = {}
+    rtt_list = []
+    snd_cwnd_list = []
+    bits_per_second_list = []
+    retransmits_list = []
+    flow_table["rtt"] = rtt_list
+    flow_table["snd_cwnd"] = snd_cwnd_list
+    flow_table["bits_per_second"] = bits_per_second_list
+    flow_table["retransmits"] = retransmits_list
+    #   Add entry
+    result[0] = flow_table
 
     # Load data for each flow
     for i in range(len(intervals)):
         for j in range(len(intervals[i]['streams'])):
-            ID = intervals[i]['streams'][j]['socket']
             #   Convert rtt from us to ms
-            result[ID]["rtt"].append(intervals[i]['streams'][j]['rtt'] / 1000.0)
+            result[0]["rtt"].append(intervals[i]['streams'][j]['rtt'] / 1000.0)
             #   Convert from bit to Kbits
-            result[ID]["snd_cwnd"].append(
-                intervals[i]['streams'][j]['snd_cwnd'] / 1024)
+            result[0]["snd_cwnd"].append(float(
+                intervals[i]['streams'][j]['snd_cwnd'] / 1024.0))
             #   Convert from bit to Mbit
-            result[ID]["bits_per_second"].append(
+            result[0]["bits_per_second"].append(
                 intervals[i]['streams'][j]['bits_per_second'] / (1024 * 1024))
-            result[ID]["retransmits"].append(
+            result[0]["retransmits"].append(
                 intervals[i]['streams'][j]['retransmits'])
 
     return result
@@ -84,48 +82,79 @@ def get_socket_keys(intervals):
     return socket_keys
 
 
-def draw_charts(result, socket_keys):
+def draw_charts(result, labels, socket_keys):
     """
     Draw line charts for rtt, snd_cwnd, bits_per_second and retransmits.
 
-    :param  result:         Result in dict
-    :param  socket_keys:    Socket IDs in list
     """
     colors = ["#fd6126", "#029ED9", "#81FF38", "#FF7438", "#FF4238", "#A3159A"]
-    size = 2.0
+    size = 1
+    styles = ["o", "."]
+
     #   Get the lengthe of Y stick
     y = []
     for i in range(len(result[socket_keys[0]]['rtt'])):
         y.append(i)
 
-    # Draw N lines in the same chart
-    plt.subplot(2, 2, 1)
-    plt.title("Bits per second")
+    # get jain`s fairness index
+    band_0 = result[socket_keys[0]]['bits_per_second']
+    band_1 = result[socket_keys[1]]['bits_per_second']
+    index = []
+    for i in range(len(band_0)):
+        x0 = band_0[i]
+        x1 = band_1[i]
+        if x0 == 0:
+            x0 = 0.1
+        if x1 == 0:
+            x1 = 0.1
+        index.append(((x0 + x1) * (x0 + x1)) / (2 * ((x0 * x0) + (x1 * x1))))
+
+    # bandwidth
+    band_fig = plt.figure("bandwidth")
     plt.ylabel("Bandwidth(Mbits/sec)")
     for i in range(len(socket_keys)):
         plt.plot(y, result[socket_keys[i]]['bits_per_second'],
-                 colors[i], label=socket_keys[i], linewidth=size)
+                 colors[i], label=labels[i], linewidth=size)
+    plt.legend()
+    band_fig.savefig("bandwidth.pdf", format="pdf")
+    band_fig.show()
 
-    plt.subplot(2, 2, 2)
-    plt.title("Retransmitted packet number")
+    # retransmit
+    retr_fig = plt.figure("retransmit")
     plt.ylabel("Packets number")
     for i in range(len(socket_keys)):
         plt.plot(y, result[socket_keys[i]]['retransmits'],
-                 colors[i], label=socket_keys[i], linewidth=size)
+                 colors[i], label=labels[i], linewidth=size)
+    plt.legend()
+    retr_fig.savefig("retr.pdf", format="pdf")
+    retr_fig.show()
 
-    plt.subplot(2, 2, 3)
-    plt.title("Congestion window size")
+    # congestion window size
+    cong_fig = plt.figure("cong")
     plt.ylabel("Size(KB)")
     for i in range(len(socket_keys)):
         plt.plot(y, result[socket_keys[i]]['snd_cwnd'],
-                 colors[i], label=socket_keys[i], linewidth=size)
+                 colors[i], label=labels[i], linewidth=size)
+    plt.legend()
+    cong_fig.savefig("cong.pdf", format="pdf")
+    cong_fig.show()
 
-    plt.subplot(2, 2, 4)
-    plt.title("RTT")
+    # rtt
+    rtt_fig = plt.figure("rtt")
     plt.ylabel("ms")
     for i in range(len(socket_keys)):
         plt.plot(y, result[socket_keys[i]]['rtt'],
-                 colors[i], label=socket_keys[i], linewidth=size)
+                 colors[i], label=labels[i], linewidth=size)
+    plt.legend()
+    rtt_fig.savefig("rtt.pdf", format="pdf")
+    rtt_fig.show()
+
+    # fairness index
+    rtt_fig = plt.figure("index")
+    plt.plot(y, index, colors[i], label="Fariness Index", linewidth=size)
+    plt.legend()
+    rtt_fig.savefig("index.pdf", format="pdf")
+    rtt_fig.show()
 
     plt.show()
 
@@ -162,7 +191,8 @@ def get_statistics(result, socket_keys):
         bandwidth = result[socket_keys[i]]["bits_per_second"]
         print (
             "[%-2d]   %-10.2f%-10.2f%-10.2f%-10.2f%-10.2f" % (
-                socket_keys[i], numpy.max(bandwidth), numpy.min(bandwidth), numpy.median(bandwidth), numpy.average(bandwidth), numpy.var(bandwidth)))
+                socket_keys[i], numpy.max(bandwidth), numpy.min(bandwidth), numpy.median(bandwidth),
+                numpy.average(bandwidth), numpy.var(bandwidth)))
 
     # Retransmission Ratio
     print "--------------------------------------------------------"
@@ -171,7 +201,8 @@ def get_statistics(result, socket_keys):
         retr = result[socket_keys[i]]["retransmits"]
         print (
             "[%-2d]   %-10.2f%-10.2f%-10.2f%-10.2f%-10.2f" % (
-                socket_keys[i], numpy.max(retr), numpy.min(retr), numpy.median(retr), numpy.average(retr), numpy.var(retr)))
+                socket_keys[i], numpy.max(retr), numpy.min(retr), numpy.median(retr), numpy.average(retr),
+                numpy.var(retr)))
 
     # BDP
     print "--------------------------------------------------------"
@@ -184,7 +215,8 @@ def get_statistics(result, socket_keys):
         for j in range(len(rtt)):
             bdp.append(rtt[j] * bandwidth[j])
 
-        print ("[%-2d]   %-10.2f%-10.2f%-10.2f%-10.2f%-10.2f" % (socket_keys[i], numpy.max(bdp), numpy.min(bdp), numpy.median(bdp), numpy.average(bdp), numpy.var(bdp)))
+        print ("[%-2d]   %-10.2f%-10.2f%-10.2f%-10.2f%-10.2f" % (
+        socket_keys[i], numpy.max(bdp), numpy.min(bdp), numpy.median(bdp), numpy.average(bdp), numpy.var(bdp)))
 
 
 def parse_arguments():
@@ -206,23 +238,27 @@ def main():
     Main Function, nothing to comment
     """
     #   Load and parse json object from file with specific
-    file_name = "./LabRecord/result/true_topo/aliyun1_to_amazon/benchmark/benchmark.log"
-    doc = re.sub("[\n|\t]", "", "".join(read_text_file(file_name)))
-    json_object = json.loads("".join(doc))
+    labels = ["Hybla", "Reno"]
+    experimented = "./LabRecord/result/fairness/international/vs_reno/hybla/3/hybla.log"
+    contrasted = "./LabRecord/result/fairness/international/vs_reno/hybla/3/reno.log"
+
+    doc_experimented = re.sub("[\n|\t]", "", "".join(read_text_file(experimented)))
+    doc_contrasted = re.sub("[\n|\t]", "", "".join(read_text_file(contrasted)))
+
+    json_experimented = json.loads("".join(doc_experimented))
+    json_contrasted = json.loads("".join(doc_contrasted))
 
     #   Important JSON objects
-    intervals = json_object["intervals"]
-    start = json_object["start"]
-    end = json_object["end"]
+    intervals_exp = json_experimented["intervals"]
+    intervals_con = json_contrasted["intervals"]
 
     #   Socket IDs to get data from result dictionary
-    socket_keys = get_socket_keys(intervals)
+    result_exp = get_result_dictionary(intervals_exp)
+    result_con = get_result_dictionary(intervals_con)
 
-    result = get_result_dictionary(intervals, socket_keys)
+    result = {0: result_exp[0], 1: result_con[0]}
 
-    get_statistics(result, socket_keys)
-
-    draw_charts(result, socket_keys)
+    draw_charts(result, labels, socket_keys=[0, 1])
 
     return 0
 
