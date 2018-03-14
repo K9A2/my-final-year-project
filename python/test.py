7fc19952031431d76044aeea9586129fdf2cdcb3
stormlin <376997313@qq.com>
2018-03-14 20:39:02 +0800

    refactor[./python]: move all python scripts to this folder.

--- python/test.py
+++ python/test.py
@@ -3,7 +3,7 @@ import numpy as np
 mu = 0
 sigma = 1
 x = mu + sigma*np.random.randn(10000)
-fig,(ax0,ax1)=plt.subplots(ncols=2, figsize=(9,6))
+fig, (ax0, ax1) = plt.subplots(ncols=2, figsize=(9, 6))
 ax0.hist(x, 20, normed=1, histtype='bar', facecolor='g', alpha=0.75)
 ax0.set_title('pdf')
 ax1.hist(x, 20, normed=1, histtype='bar', rwidth=0.8, cumulative=True)
